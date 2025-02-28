75010cd7422c32e29bddd16f9960c44a39060400
Mateusz <chudy3133@gmail.com>
2025-02-28 10:35:29 +0100

    5

---
+++ src/app/[lang]/login/[[...sign-up]]/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { SignUp } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+
+export default function SignUpPage() {
+  const params = useParams();
+  const lang = params?.lang as string;
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+      <SignUp 
+        afterSignUpUrl={`/${lang}/admin`}
+        appearance={{
+          elements: {
+            rootBox: "bg-white p-8 rounded-lg shadow-xl",
+            card: {
+              boxShadow: "none",
+              width: "400px", 
+              margin: "0"
+            },
+            formButtonPrimary: 
+              "bg-blue-500 hover:bg-blue-600 text-sm normal-case",
+          },
+        }}
+      />
+    </div>
+  );
+}
